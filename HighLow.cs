438bcc43491a1fc8f353c86e21c8b7fd287fc373
Scott Bergler and Victoria Martinez <scott@>
2018-11-13 17:08:36 -0800

    add basic methods and beginning UI.

---
+++ HighLow.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace HigherLowerGame {
+
+    class HighLow
+    {
+        private int GuessRangeLow;
+        private int GuessRangeHigh;
+        private int GameGuess;
+
+        public HighLow(int guessRangeLow, int guessRangeHigh)
+        {
+            GuessRangeLow = guessRangeLow;
+            GuessRangeHigh = guessRangeHigh;
+        }
+
+        public int FindNextGuessRange()
+        {
+            if((GuessRangeHigh - GuessRangeLow) % 2 == 0)
+            {
+                GameGuess = (GuessRangeHigh - GuessRangeLow) / 2 + GuessRangeLow;
+            }
+            else
+            {
+                GameGuess = ((GuessRangeHigh - GuessRangeLow) + 1) / 2 + GuessRangeLow;
+            }
+            return GameGuess;
+        }
+
+        public int SetNewGuessRange(string userResponse)
+        {
+            if (userResponse == "higher")
+            {
+                GuessRangeLow = GameGuess + 1;
+            }
+            else if (userResponse == "lower")
+            {
+                GuessRangeHigh = GameGuess - 1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace HigherLowerGame {
+
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(" Would you like to play the higher/lower game? (Yes/No)");
+            string answerToPlayGame = Console.ReadLine();
+            if (answerToPlayGame == "No" || answerToPlayGame == "no")
+            {
+                Console.WriteLine("Goodbye.");
+            }
+
+            Console.WriteLine("Okay.");
+
+            HighLow newGame = new HighLow(0, 100);
+
+            Console.WriteLine("Guess a number from 1 to 100. Don't tell anyone what it is.");
+
+            int gameGuess = newGame.FindNextGuessRange();
+            // Console.WriteLine(gameGuess);
+            Console.WriteLine("Is your number higher or lower than " + gameGuess + "? (higher/lower/correct)");
+            string userResponse = Console.ReadLine();
+
+            if (userResponse == "higher" || userResponse == "lower" )
+            {
+                newGame.SetNewGuessRange(userResponse);
+                gameGuess = newGame.FindNextGuessRange();
+
+                Console.WriteLine("Is your number higher or lower than " + gameGuess + "? (higher/lower/correct)");
+                userResponse = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("I win!");
+            }
+        }
+    }
+}
